6ed7eb2c8c99410b2a11294de194b979a7ab27fb
Alek30k <alemank23k@gmail.com>
2022-12-14 23:33:44 -0300

    add register/style/react-router-dom

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,31 @@
 import "./register.scss";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>Ale Social.</h1>
+          <p>
+            Lorem ipsum dolor beatae et? impedit voluptatibus numquam tenetur
+            pariatur labore.
+          </p>
+          <span>Do you have an account?</span>
+          <button>Login</button>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="text" placeholder="Name" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
